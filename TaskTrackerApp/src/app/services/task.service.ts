9dc092352e063c93026f51ec3104bfb6ff3283e0
Chukwuka1488 <juniorakibor@yahoo.com>
2021-07-06 18:22:22 +0200

    Angular HTTP Client and fetch tasks functionality added

--- TaskTrackerApp/src/app/services/task.service.ts
+++ TaskTrackerApp/src/app/services/task.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@angular/core';
+// import http client to connect backend to frontend
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+// @Injectable({providedIn: 'root'})
+// export class ServiceNameService {
+//   constructor(private httpClient: HttpClient) { }
+
+// }
 import { Observable, of } from 'rxjs';
 // the task interface or model
 import { Task } from '../Task';
-// import the task data
-import { TASKS } from '../mock-task';
+// // import the task data
+// import { TASKS } from '../mock-task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:5000/tasks'
+
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
